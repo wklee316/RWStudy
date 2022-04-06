0b30255a24b975d719316413e58844c1deb336d1
sangjun2002@gmail.com <sangjun2002@gmail.com>
2022-04-06 11:08:40 +0900

    하노이 버튼 구현

---
+++ Lecture04/main.js
@@ -0,0 +1,15 @@
+var line1 = document.getElementById("first");
+var line2 = document.getElementById("second");
+var line3 = document.getElementById("third");
+var line4 = document.getElementById("fourth");
+var line5 = document.getElementById("fifth");
+var line6 = document.getElementById("sixth");
+
+function init(){
+    line1.innerHTML = "🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥 🟥";
+    line2.innerHTML = "🟨 🟨 🟨 🟨 🟨 🟨 🟨 🟨 🟨";
+    line3.innerHTML = "🟩 🟩 🟩 🟩 🟩 🟩 🟩";
+    line4.innerHTML = "🟦 🟦 🟦 🟦 🟦";
+    line5.innerHTML = "🟪 🟪 🟪";
+    line6.innerHTML = "⬛"
+}
